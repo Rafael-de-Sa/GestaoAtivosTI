591b7e78d65066763765357f342ef232adf605b4
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-26 16:36:33 -0300

    :sparkles: feat: Classe SistemaControleBensTI

---
+++ src/gestaoativosti/SistemaControleBensTI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestaoativosti;
+
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+
+/*
+Crie uma classe SistemaControleBensTI com métodos para:
+Adicionar ativos ao sistema.
+Adicionar funcionários ao sistema.
+Vincular um ativo a um funcionário.
+Listar todos os ativos e os funcionários aos quais estão vinculados.
+ */
+public class SistemaControleBensTI {
+}
