a44b7c3f98bb92172a99d3efd6db14c32649d88a
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-26 14:39:17 -0300

    :sparkles: feat: Adicionado Classe Computador

---
+++ src/gestaoativosti/Computador.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestaoativosti;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Computador extends AtivoTI {
+
+    private String especificacoes;
+
+    public Computador(String nome, int id) {
+        super(nome, id);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Computador";
+    }
+
+    public String getEspecificacoes() {
+        return especificacoes;
+    }
+
+    public void setEspecificacoes(String especificacoes) {
+        this.especificacoes = especificacoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Computador{" + super.toString() + "especificacoes=" + especificacoes + '}';
+    }
+
+}
