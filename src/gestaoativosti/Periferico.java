85f0a67fc2b725f043a2368ba0f734831262ade7
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-26 14:39:38 -0300

    :sparkles: feat: Adicionado Classe Periferico

---
+++ src/gestaoativosti/Periferico.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestaoativosti;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Periferico extends AtivoTI {
+
+    private String tipo;
+
+    public Periferico(String nome, int id) {
+        super(nome, id);
+    }
+
+    public String getTipoPeriferico() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Periferico (" + this.getTipoPeriferico() + ")";
+    }
+
+    @Override
+    public String toString() {
+        return "Periferico{" + super.toString() + "tipo=" + tipo + '}';
+    }
+
+}
