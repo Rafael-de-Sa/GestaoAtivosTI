d703d7097185c816e85f0fced38ff21413c355ee
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-26 14:40:09 -0300

    :sparkles: feat: Adicionado Classe Funcionarios

---
+++ src/gestaoativosti/Funcionario.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestaoativosti;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Funcionario {
+
+    private String nome;
+    private int id;
+
+    public Funcionario(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" + "nome=" + nome + ", id=" + id + '}';
+    }
+
+}
